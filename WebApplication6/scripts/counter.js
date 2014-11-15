9abcc0df9107c77fc7ebb2afc99fb81d1901e4c2
wsyperek <wsyperek@gmail.com>
2014-11-15 20:06:34 +0100

    First

---
+++ WebApplication6/scripts/counter.js
@@ -0,0 +1,38 @@
+﻿var d = (new Date);
+var h = d.getHours();
+var m = d.getMinutes();
+var s = d.getSeconds();
+var zeit = h + ":" + m + ":" + s;
+
+function Person(name, vorname, alter) {
+    
+}
+
+
+
+
+var counterButtonOnClick = function () {
+    var button = document.getElementsByName("counterButton")[0];
+    var jetzt = new Date();
+    jetzt = jetzt.getTime();
+    var seitStartVerstrichen = jetzt - d.getTime();
+    var seitStartVerstrichenInSekunden = TS.GetSecondsFromMiliseconds(seitStartVerstrichen);
+    var myTS1 = TS;
+    var myTS2 = TS;
+    myTS1.Name = "Frank";
+    myTS2.Name = "Holger";
+    button.textContent = seitStartVerstrichenInSekunden;
+
+};
+
+var TS = {
+    Name:"Peter",
+    GetSecondsFromMiliseconds: function (miliseconds) {
+        var n = TS.Name;
+        return miliseconds / 1000;
+    },
+
+}
+
+
+
